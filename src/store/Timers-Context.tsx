63a29f4a48118f8c4f8ad0d77237f785e427900d
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-04-15 18:48:34 +0330

    add reducer to make context api interactive

--- src/store/Timers-Context.tsx
+++ src/store/Timers-Context.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { type ReactNode, createContext, useContext } from "react";
+import { type ReactNode, createContext, useContext, useReducer } from "react";
 
 type Timer = {
   name: string;
@@ -16,6 +16,47 @@ type TimersContextValue = TimersState & {
   startTimer: () => void;
   stopTimer: () => void;
 };
+// ********************for reducer
+const initialState: TimersState = {
+  isRunning: true,
+  timers: [],
+};
+
+type AddTimerAction = {
+  type: "ADD_TIMER";
+  payload: Timer;
+};
+type StartTimerAction = {
+  type: "START_TIMER";
+};
+type StopTimerAction = {
+  type: "STOP_TIMER";
+};
+
+type Action = {
+  type: AddTimerAction | StartTimerAction | StopTimerAction;
+};
+
+const timersReducer = (state: TimersState, action: Action): TimersState => {
+  switch (action.type) {
+    case "ADD_TIMER":
+      return {
+        ...state,
+        timers: [
+          ...state.timers,
+          { name: action.payload.name, duration: action.payload.duration },
+        ],
+      };
+    case "START_TIMER":
+      return { ...state, isRunning: true };
+    case "STOP_TIMER":
+      return { ...state, isRunning: false };
+    default:
+      return state;
+  }
+};
+
+// ********************************
 
 const TimersContext = createContext<TimersContextValue | null>(null);
 
@@ -30,17 +71,18 @@ type TimersContextProviderProps = {
 };
 
 const TimersContextProvider = ({ children }: TimersContextProviderProps) => {
+  const [timerState, dispatch] = useReducer(timersReducer, initialState);
   const ctx: TimersContextValue = {
-    timers: [],
-    isRunning: false,
+    timers: timerState.timers,
+    isRunning: timerState.isRunning,
     addTimer(timerData) {
-      //   dispatch({ type: "ADD_TIMER" });
+      dispatch({ type: "ADD_TIMER", payload: timerData });
     },
     startTimer() {
-      //   dispatch({ type: "START_TIMER" });
+      dispatch({ type: "START_TIMER" });
     },
     stopTimer() {
-      //   dispatch({ type: "STOP_TIMER" });
+      dispatch({ type: "STOP_TIMER" });
     },
   };
 
